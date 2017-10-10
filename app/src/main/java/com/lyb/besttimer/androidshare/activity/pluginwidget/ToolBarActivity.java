2cd8a8fad18d710b6b9a1ef7b7a598a437e29a6e
linyibiao <qb20090921>
2017-10-10 11:02:55 +0800

    临时提交

--- app/src/main/java/com/lyb/besttimer/androidshare/activity/pluginwidget/ToolBarActivity.java
+++ app/src/main/java/com/lyb/besttimer/androidshare/activity/pluginwidget/ToolBarActivity.java
@@ -1,7 +1,9 @@
 package com.lyb.besttimer.androidshare.activity.pluginwidget;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
@@ -10,14 +12,15 @@ import com.lyb.besttimer.androidshare.activity.BaseActivity;
 
 public class ToolBarActivity extends BaseActivity {
 
+    private Toolbar toolB;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tool_bar);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolB);
-        setSupportActionBar(toolbar);
-        toolbar.inflateMenu(R.menu.menu_toolbar);
-        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+        toolB = (Toolbar) findViewById(R.id.toolB);
+        setSupportActionBar(toolB);
+        toolB.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
                 switch (item.getItemId()) {
@@ -40,4 +43,15 @@ public class ToolBarActivity extends BaseActivity {
         });
     }
 
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        toolB.setTitle("这个标题才能最终起作用");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 }
