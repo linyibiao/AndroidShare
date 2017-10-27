60eb3f7313fe4ac0bdb7b6b5785e0c0b5acdac1c
Besttimer <295243617@qq.com>
2017-10-28 01:12:31 +0800

    临时提交

--- app/src/main/java/com/lyb/besttimer/androidshare/activity/pluginwidget/EditTextActivity.java
+++ app/src/main/java/com/lyb/besttimer/androidshare/activity/pluginwidget/EditTextActivity.java
@@ -1,12 +1,15 @@
 package com.lyb.besttimer.androidshare.activity.pluginwidget;
 
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.widget.EditText;
 
 import com.lyb.besttimer.androidshare.R;
+import com.lyb.besttimer.pluginwidget.utils.LogUtil;
 
 public class EditTextActivity extends AppCompatActivity {
 
@@ -16,6 +19,9 @@ public class EditTextActivity extends AppCompatActivity {
         setContentView(R.layout.activity_edit_text);
         final EditText editText = (EditText) findViewById(R.id.et);
         editText.addTextChangedListener(new TextWatcher() {
+
+            private boolean hadSet=false;
+
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
@@ -28,6 +34,23 @@ public class EditTextActivity extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable s) {
+                editText.setLineSpacing(0,0);
+                if (!hadSet&&editText.getLineCount()>=3){
+                    int maxHeight=0;
+                    Rect bounds=new Rect();
+                    editText.getLineBounds(0,bounds);
+                    maxHeight+=bounds.height()+bounds.top;
+                    LogUtil.logE("0000  "+bounds);
+                    editText.getLineBounds(1,bounds);
+                    maxHeight+=bounds.height();
+                    LogUtil.logE("0001  "+bounds);
+                    editText.getLineBounds(editText.getLineCount()-1,bounds);
+                    maxHeight+=bounds.height()+(editText.getHeight()-bounds.bottom);
+                    LogUtil.logE("0002  "+bounds);
+                    editText.setMaxHeight(maxHeight);
+                    hadSet=true;
+                }
+//                LogUtil.logE(editText.getLineCount()+"  "+editText.getLineHeight()+"  "+editText.getLineBounds(0,null)+"  "+editText.getLineSpacingMultiplier());
                 if (s.toString().equals("hello")) {
                     editText.setError("what");
                 }
