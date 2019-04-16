c06d9896095cd4aea8ee7fba99dba029fe8147a9
linyibiao <qb20090921>
2019-04-16 17:49:28 +0800

    camera发布

--- cameracore/src/main/java/com/lyb/besttimer/cameracore/activity/CameraMixActivity.java
+++ cameracore/src/main/java/com/lyb/besttimer/cameracore/activity/CameraMixActivity.java
@@ -1,5 +1,6 @@
 package com.lyb.besttimer.cameracore.activity;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
@@ -7,6 +8,7 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 import com.lyb.besttimer.cameracore.CameraConstants;
 import com.lyb.besttimer.cameracore.CameraResultCaller;
@@ -17,15 +19,22 @@ import com.lyb.besttimer.cameracore.fragment.CameraShowFragment;
 import com.lyb.besttimer.pluginwidget.utils.FragmentUtil;
 import com.lyb.besttimer.pluginwidget.view.loading.LoadingCaller;
 import com.lyb.besttimer.pluginwidget.view.loading.LoadingView;
+import com.tbruyelle.rxpermissions2.RxPermissions;
+
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
 
 public class CameraMixActivity extends AppCompatActivity implements CameraResultCaller {
 
     private ActivityCameraMixBinding binding;
 
+    private RxPermissions rxPermissions;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_camera_mix);
+        rxPermissions = new RxPermissions(this);
         showCamera();
         binding.loadvGo.setLoadingCaller(new LoadingCaller() {
 
@@ -113,9 +122,19 @@ public class CameraMixActivity extends AppCompatActivity implements CameraResult
     }
 
     private void showCamera() {
-        FragmentUtil.replace(getSupportFragmentManager(), R.id.layout_show, CameraFragment.class, null, null);
-        binding.layoutCapture.setVisibility(View.VISIBLE);
-        binding.layoutCheck.setVisibility(View.GONE);
+        Disposable disposable = rxPermissions.request(Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.RECORD_AUDIO).subscribe(new Consumer<Boolean>() {
+            @Override
+            public void accept(Boolean aBoolean) throws Exception {
+                if (aBoolean) {
+                    FragmentUtil.replace(getSupportFragmentManager(), R.id.layout_show, CameraFragment.class, null, null);
+                    binding.layoutCapture.setVisibility(View.VISIBLE);
+                    binding.layoutCheck.setVisibility(View.GONE);
+                } else {
+                    Toast.makeText(CameraMixActivity.this, "请开启相机相关权限", Toast.LENGTH_SHORT).show();
+                    onBackPressed();
+                }
+            }
+        });
     }
 
     private void showCameraPreview(String fileUrl, ResultType resultType) {
